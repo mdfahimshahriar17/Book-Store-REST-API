e9890d7f56e3b0cc0fec332d8f5cc40e3d1e2411
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-09-01 16:33:07 +0600

    Created  Serializer for BookList and Views, Urls

---
+++ book/API/views.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+
+from book import models
+from . import serializer
+
+class BookListViewSet(viewsets.ModelViewSet):
+    queryset = models.Book.objects.all()
+    serializer_class = serializer.BookListSerializer
